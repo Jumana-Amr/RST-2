a70b3f0192fbf3d6c40288fff5ec015ae1724d0a
JumanaAmr <jumana.amr@ocsbstudent.ca>
2021-03-03 14:52:11 +0000

    Trial 3

--- graph.js
+++ graph.js
@@ -1,101 +1,81 @@
-var canvas
-var context
+let canvas
+let context
 
 window.onload = function () {
   canvas = document.getElementById('plot-canvas')
   context = canvas.getContext('2d')
 }
-
-function plot ()
-{ // eslint-disable-line no-unused-vars
-  var a = slope
-	var b = yinter
-
-
-	document.getElementById('fun-equation').innerHTML = 'y=' + a +'x+' + b
-
-	context.clearRect(0,0, canvas.width, canvas.height)
-
-
-	var x0 = 0.5 * canvas.width
-	var y0 = 0.5 * canvas.height
-	var x
-  var scale = 0.2
-	var y
-	var dx = 4
-	var xMax = Math.round((canvas.width-x0)/dx)
-	var xMin = Math.round(-x0/dx)
-	var axes={}
-	axes.x0 = x0
-	axes.y0 = y0
-	axes.scale = scale
-
-	drawAxes(context,axes)
-	context.beginPath()
-	context.strokeStyle = 'red'
-	context.lineWidth = 2
-	for (var i=xMin; i<xMax; i++) {
-		x=dx*i
-		y=(a*x+b)
-		
-		x /= scale
-		y /= scale
-		if(i==xMin) {
-			context.moveTo(x0+x,y0-y)
-		} else {
-			context.lineTo(x0+x,y0-y)
-		}
-	}
-
+function plot () {// eslint-disable-line no-unused-vars
+  let a = slope
+  let b = yinter
+  document.getElementById('fun-equation').innerHTML = 'y=' + a +'x+' + b
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  let x0 = 0.5 * canvas.width
+  let y0 = 0.5 * canvas.height
+  let x
+  let scale = 0.2
+  let y
+  let dx = 4
+  let xMax = Math.round((canvas.width - x0) / dx)
+  let xMin = Math.round(- x0 / dx)
+  let axes={}
+  axes.x0 = x0
+  axes.y0 = y0
+  axes.scale = scale
+  drawAxes(context,axes)
+  context.beginPath()
+  context.strokeStyle = 'red'
+  context.lineWidth = 2
+  for (let i = xMin; i < xMax; i++) {
+    x = dx * i
+    y = (a * x + b)
+    x /= scale
+    y /= scale
+    if(i === xMin) {
+      context.moveTo(x0 + x, y0 - y)
+    } else {
+      context.lineTo( x0 + x, y0 - y)
+    }
+  }
 	context.stroke()
 }
 
-function drawAxes(context, axes)
-{
-	var x0=axes.x0
-	var y0=axes.y0
-	var width=context.canvas.width
-	var height = context.canvas.height
-	var xmin = 0
-  var xaxis = 0
-  var xaxis2 = 0
-  var yaxis = 0
-  var yaxis2 = 0
-	context.beginPath()
-	context.strokeStyle = 'black'
-	context.lineWidth = 1
-	//----Y axis----
-	context.moveTo(xmin, y0)
-	context.lineTo(width, y0)
-	//----X axis-----
-	context.moveTo(x0,0)
-	context.lineTo(x0,height)
-
-
-
-	// X - signs
-  for(var i=x0; i<width; i+=50)
-  {
-    context.strokeText (xaxis,i,height/2,20)
-    xaxis+=10
+function drawAxes(context, axes) {// eslint-disable-line no-unused-vars
+  let x0=axes.x0
+  let y0=axes.y0
+  let width=context.canvas.width
+  let height = context.canvas.height
+  let xmin = 0
+  let xaxis = 0
+  let xaxis2 = 0
+  let yaxis = 0
+  let yaxis2 = 0
+  context.beginPath()
+  context.strokeStyle = 'black'
+  context.lineWidth = 1
+  //----Y axis----
+  context.moveTo(xmin, y0)
+  context.lineTo(width, y0)
+  //----X axis-----
+  context.moveTo(x0, 0)
+  context.lineTo(x0, height)
+  // X - signs
+  for (let i = x0; i < width; i += 50) {
+    context.strokeText (xaxis, i, height / 2, 20)
+    xaxis += 10
   }
-  for(var i=x0; i>0; i-=50)
-  {
-    context.strokeText (xaxis2,i,height/2,20)
-    xaxis2-=10
+  for (let i = x0; i > 0; i -= 50) {
+    context.strokeText (xaxis2, i, height / 2, 20)
+    xaxis2 -= 10
   }
-  
   // Y - signs
-  for(var j=y0; j<height; j+=50)
-  {
-    context.strokeText (yaxis,(width/2),j,20)
-    yaxis-=10
+  for (let j = y0; j < height; j += 50) {
+    context.strokeText (yaxis, (width / 2), j, 20)
+    yaxis -= 10
   }
-  for(var j=y0; j>0; j-=50)
-  {
-    context.strokeText (yaxis2,(width/2),j,20)
-    yaxis2+=10
+  for (let j = y0; j > 0; j -= 50) {
+    context.strokeText (yaxis2, (width / 2), j, 20)
+    yaxis2 += 10
   }
-
-	context.stroke()
+  context.stroke()
 }
--- script.js
+++ script.js
@@ -10,17 +10,20 @@ function updateForm () { // eslint-disable-line no-unused-vars
   document.getElementById('id-output-x-intercept1').hidden = true
   switch (functions) {
     case 'slope':
-    document.getElementById('id-inputs-slope').hidden = false
-    break
+      document.getElementById('id-inputs-slope').hidden = false
+      break
     case 'standard':
-    document.getElementById('id-inputs-standard').hidden = false
-    break
+      document.getElementById('id-inputs-standard').hidden = false
+      break
   }
 }
 
 let xinter = 0
 let yinter = 0
 let slope = 0
+let xinter1 = 0
+let yinter1 = 0
+let slope1 = 0
 function calculateEquationLin () { // eslint-disable-line no-unused-vars
   const functions = document.getElementById('id-functions').value
   switch (functions) {
@@ -34,29 +37,31 @@ function calculateEquationLin () { // eslint-disable-line no-unused-vars
       const y2 = document.getElementById('id-y2').value
       slope = (y2 - y1) / (x2 - x1)
       yinter = y2 - slope * x2
-      xinter = - (yinter) / slope
+      xinter = -(yinter) / slope
       document.getElementById('id-output-slope').innerHTML = 'Slope = ' + slope
-      document.getElementById('id-output-y-intercept').innerHTML  = 'Y-intercept = ' + yinter
+      document.getElementById('id-output-y-intercept').innerHTML = 'Y-intercept = ' + yinter
       document.getElementById('id-output-x-intercept').innerHTML = 'X-intercept = ' + xinter
       break
     case 'standard':
-    document.getElementById('id-inputs-standard').hidden = false
+      document.getElementById('id-inputs-standard').hidden = false
       document.getElementById('id-output-slope1').hidden = false
       document.getElementById('id-output-y-intercept1').hidden = false
       document.getElementById('id-output-x-intercept1').hidden = false
-        const a = document.getElementById('id-a').value
-        const b = document.getElementById('id-b').value
-        const c = document.getElementById('id-c').value
-        slope1 = -(a) / b
-        yinter1 = c / b
-        xinter1 = c / a
-        document.getElementById('id-output-slope1').innerHTML = 'Slope = ' + slope
-        document.getElementById('id-output-y-intercept1').innerHTML = 'y-intercept = ' + yinter1
-        document.getElementById('id-output-x-intercept1').innerHTML = 'x-intercept = ' + xinter1
-        break
+      const a = document.getElementById('id-a').value
+      const b = document.getElementById('id-b').value
+      const c = document.getElementById('id-c').value
+      slope1 = -(a) / b
+      yinter1 = c / b
+      xinter1 = c / a
+      document.getElementById('id-output-slope1').innerHTML = 'Slope = ' + slope
+      document.getElementById('id-output-y-intercept1').innerHTML = 'y-intercept = ' + yinter1
+      document.getElementById('id-output-x-intercept1').innerHTML = 'x-intercept = ' + xinter1
+      break
   }
 }
 
+let solvex = 0
+let solvey = 0
 function calculateEquationLin2 () { // eslint-disable-line no-unused-vars
     document.getElementById('id-output-solvex').hidden = false
     document.getElementById('id-output-solvey').hidden = false
@@ -68,10 +73,24 @@ function calculateEquationLin2 () { // eslint-disable-line no-unused-vars
       const ce2 = document.getElementById('id-ce2').value
       solvex = ((ce2 * ye1) - (ce1 * ye2)) / ((xe2 * ye1) - (xe2) - (xe1))
       solvey = ( ce1 - (xe1 * solvex)) / (ye1)
+      slope1 = -(xe1) / ye1
+      yinter1 = ce1 / ye1
+      slope2 = -(xe2) / ye2
+      yinter2 = ce2 / ye2
       document.getElementById('id-output-solvex').innerHTML = 'x = ' + solvex
       document.getElementById('id-output-solvey').innerHTML = 'y = ' + solvey
+      document.getElementById('id-output-slope').innerHTML = 'Slope = ' + slope1
+      document.getElementById('id-output-y-intercept').innerHTML  = 'Y-intercept = ' + yinter1
+      document.getElementById('id-output-slope').innerHTML = 'Slope = ' + slope2
+      document.getElementById('id-output-y-intercept').innerHTML  = 'Y-intercept = ' + yinter2
   }
 
+let discriminant = 0
+let root1 = 0
+let root2 = 0
+let aos = 0
+let xvertex = 0
+let yvertex = 0
 function calculateEquationquad () {
   document.getElementById('id-output-discriminant').hidden = false
   document.getElementById('id-output-root-1').hidden = false
@@ -80,27 +99,27 @@ function calculateEquationquad () {
     const aq = document.getElementById('id-aq').value
     const bq = document.getElementById('id-bq').value
     const cq = document.getElementById('id-cq').value
-    discriminant =  bq * bq - 4 * aq * cq
-    if (discriminant === 0 ) {
-      document.getElementById('demo').innerHTML = '1 solution'
-      document.getElementById('id-output-root-2').hidden = true
-      root1 = - (bq) + (Math.sqrt(bq * bq - 4 * aq * cq)) / (2 * aq)
-      document.getElementById('id-output-root-1').innerHTML = 'Root 1 = ' + root1
-    } else if (discriminant > 0 ) {
-      document.getElementById('demo').innerHTML = '2 solutions'
-      root1 = -(bq) + (Math.sqrt(bq * bq - 4 * aq * cq)) / (2 * aq)
-      root2 = - (bq) - (Math.sqrt(bq * bq - 4 * aq * cq)) / (2 * aq)
-      document.getElementById('id-output-root-1').innerHTML = 'Root 1 = ' + root1
-      document.getElementById('id-output-root-2').innerHTML = 'Root 2 = ' + root2
-    } else {
-      document.getElementById('demo').innerHTML = 'no solution'
-      document.getElementById('id-output-root-1').hidden = true
-      document.getElementById('id-output-root-2').hidden = true
-    }
-    aos = -(bq / 2 * aq)
-    xvertex = -(bq) / 2 * aq
-    yvertex = xvertex * xvertex + bq * ( xvertex ) + cq
-    document.getElementById('id-output-discriminant').innerHTML = 'Discriminant = ' + discriminant
-    document.getElementById('id-output-aos').innerHTML = 'Axis of Symetry = ' + aos
-    document.getElementById('id-output-vertex').innerHTML = 'vertex = ( ' + xvertex + ' , ' + yvertex + ' )'
+    discriminant = bq * bq - 4 * aq * cq
+  if (discriminant === 0 ) {
+    document.getElementById('demo').innerHTML = '1 solution'
+    document.getElementById('id-output-root-2').hidden = true
+    root1 = - (bq) + (Math.sqrt(bq * bq - 4 * aq * cq)) / (2 * aq)
+    document.getElementById('id-output-root-1').innerHTML = 'Root 1 = ' + root1
+  } else if (discriminant > 0 ) {
+    document.getElementById('demo').innerHTML = '2 solutions'
+    root1 = -(bq) + (Math.sqrt(bq * bq - 4 * aq * cq)) / (2 * aq)
+    root2 = - (bq) - (Math.sqrt(bq * bq - 4 * aq * cq)) / (2 * aq)
+    document.getElementById('id-output-root-1').innerHTML = 'Root 1 = ' + root1
+    document.getElementById('id-output-root-2').innerHTML = 'Root 2 = ' + root2
+  } else {
+    document.getElementById('demo').innerHTML = 'no solution'
+    document.getElementById('id-output-root-1').hidden = true
+    document.getElementById('id-output-root-2').hidden = true
+  }
+  aos = -(bq / 2 * aq)
+  xvertex = -(bq) / 2 * aq
+  yvertex = xvertex * xvertex + bq * ( xvertex ) + cq
+  document.getElementById('id-output-discriminant').innerHTML = 'Discriminant = ' + discriminant
+  document.getElementById('id-output-aos').innerHTML = 'Axis of Symetry = ' + aos
+  document.getElementById('id-output-vertex').innerHTML = 'vertex = ( ' + xvertex + ' , ' + yvertex + ' )'
 }
